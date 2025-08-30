1bd273db8f8b63f06e033497a3793905aca4454c
GalacticaUA <igrogame3000@gmail.com>
2025-08-30 16:31:20 +0200

    #20 Fix handleClick

--- frontend/src/components/QrCode/QrCodeButton.jsx
+++ frontend/src/components/QrCode/QrCodeButton.jsx
@@ -4,21 +4,23 @@ import { IoQrCode } from "react-icons/io5"; // QR code icon from react-icons
 import { useQrCode } from "./QrCodeContext"; // Custom context hook to share QR state
 import styles from "./QrCodeButton.module.css"; // CSS module for styling
 
-function QrCodeButton({ shortUrl }) {
+function QrCodeButton({ shortUrl, ariaLabel = "Qr code" }) {
   // Access context functions to update modal visibility and QR image
   const { setShowQrCode, setQrUrl } = useQrCode();
 
   // Handle button click => generate QR code and show modal
   const handleClick = async () => {
+    if (!shortUrl) return;
     const dataUrl = await QRCode.toDataURL(shortUrl); // Convert shortUrl into QR code (as Data URL image)
     setQrUrl(dataUrl);                                // Save QR image into context
     setShowQrCode(true);                              // Open QR modal
   };
 
   return (
-    <button className={styles.qrCodeButton} onClick={handleClick}>
+    <button className={styles.qrCodeButton} onClick={handleClick} title={ariaLabel} aria-label={ariaLabel}>
       {/* Small QR icon inside button */}
       <IoQrCode size={15} color="black" />
+      
     </button>
   );
 }
