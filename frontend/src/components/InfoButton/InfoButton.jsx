3e0f4c6afb51f6a30e58fb69ec36d630aca3815f
GalacticaUA <igrogame3000@gmail.com>
2025-08-30 16:29:38 +0200

    #20 Add a new type to the InfoButton

--- frontend/src/components/InfoButton/InfoButton.jsx
+++ frontend/src/components/InfoButton/InfoButton.jsx
@@ -3,10 +3,10 @@ import styles from "./InfoButton.module.css";
 export default function InfoButton({ onClick }) {
   return (
     <button
+      type="button"
       className={styles.infoButton}
       onClick={onClick}
       title="Info"
-    >
-    </button>
+    ></button>
   );
 }
